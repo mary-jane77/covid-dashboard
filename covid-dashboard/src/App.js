98155ea0afd6afaa53ee1a60a497e0bccbad2c77
Elizabeth Tukaeva <elizabeth77531@gmail.com>
2020-12-10 23:03:18 +0300

    refactor: change structure of the project

---
+++ covid-dashboard/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        COVID-19 Dashboard
+      </header>
+    </div>
+  );
+}
+
+export default App;
