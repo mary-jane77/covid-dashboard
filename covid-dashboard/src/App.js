93cb56b327d1d598ed7638c0dab3362bee725d10
Tatsiana <t-tanja@bk.ru>
2020-12-14 03:03:10 +0300

    fix: changed to useEffect

--- covid-dashboard/src/App.js
+++ covid-dashboard/src/App.js
@@ -1,14 +1,61 @@
 import React from 'react';
 import './App.css';
-import Details from './components/Details';
-
+import axios from './axios';
+import Summary from './components/Summary';
 
 function App() {
+  const [totalConfirmed, setTotalConfirmed] = React.useState(0);
+  const [totalRecovered, setTotalRecovered] = React.useState(0);
+  const [totalDeaths, setTotalDeaths] = React.useState(0);
+  const [covidSummary, setCovidSummary] = React.useState({});
+  const [loading, setLoading] = React.useState(false);
 
-return (
+  React.useEffect(() => {
+    setLoading(true);
+    axios.get('/summary')
+      .then((res) => {
+        setLoading(false);
+
+        if (res.status === 200) {
+          setTotalConfirmed(res.data.Global.TotalConfirmed);
+          setTotalRecovered(res.data.Global.NewRecovered);
+          setTotalDeaths(res.data.Global.TotalDeaths);
+          setCovidSummary(res.data);
+        }
+      })
+
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  if (loading) {
+    return <p> Fetching!!! </p>;
+  }
+
+  return (
     <div className="App">
-      <Details />
-    </div>
+      <Summary
+     totalConfirmed={totalConfirmed}
+     totalRecovered={totalRecovered}
+     totalDeaths={totalDeaths}
+     covidSummary={covidSummary}
+     country={''}
+/>
+     <div>
+           {covidSummary.Countries && covidSummary.Countries.map((country) => (
+        // eslint-disable-next-line react/jsx-key
+        <tr>
+          <td>{country.Country}</td>
+          <td>
+             <td>{country.TotalConfirmed}</td>
+          </td>
+        </tr>
+           ))}
+     </div>
+
+ </div>
   );
 }
+
 export default App;
--- covid-dashboard/src/components/Summary.js
+++ covid-dashboard/src/components/Summary.js
@@ -1,39 +1,42 @@
 import React from 'react';
+/** @function Summary  */
 
+function Summary(props) {
+  const {
+    totalConfirmed,
+    totalRecovered,
+    totalDeaths,
+    country,
+  } = props;
 
-class Summary extends React.Component{
-
-   render(){
-     const {summary, currentDate} = this.props;
-      return(
-               <div className='row'>
-                  <div className = 'NewConfirmed'>
-                     <h3> New NewConfirmed</h3>
-                      <h1>{summary.NewConfirmed}</h1>
-                      <h4>{new Date(currentDate).toDateString()}</h4>
+  return (
+        < div className='row' >
+        < div className = 'NewConfirmed' >
+        < h3> Countries </h3 >
+        <h1>{country}</h1>
+                      <h4>{new Date().toDateString()}</h4>
                       </div>
                    <div className='TotalConfirmed'>
                      <h4>Total Confirmed</h4>
-                      <h1>{summary.TotalConfirmed}</h1>
+                      <h1>{totalConfirmed}</h1>
                         </div>
 
-                        <div className='NewDeaths'>
+                        {/* <div className='NewDeaths'>
                         <h4>NewDeaths</h4>
-                        <h1>{summary.NewDeaths}</h1>
-                      </div>
+                        <h1>{}</h1>
+                      </div> */}
 
-                       <div className='TotalDeaths'>
-                              <h4>TotalDeaths</h4>
-                             <h1>{summary.TotalDeaths}</h1>
-                   </div>
                       <div className='TotalRecovered'>
                            <h4>TotalRecovered</h4>
-                             <h1>{summary.TotalRecovered}</h1>
+                             <h1>{totalRecovered}</h1>
                     </div>
 
+                      <div className='TotalDeaths'>
+                              <h4>TotalDeaths</h4>
+                             <h1>{totalDeaths}</h1>
+                   </div>
             </div>
-      )
-}
 
+  );
 }
 export default Summary;
