6b410f3e8cd8e0f3862f3f5318d4639bd4668565
Elizabeth Tukaeva <elizabeth77531@gmail.com>
2020-12-23 00:12:08 +0300

    fix: footer

--- covid-dashboard/src/components/footer.jsx
+++ covid-dashboard/src/components/footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => (
       <a className="logo" href="https://rs.school/js/">
         <img src="https://rs.school/images/rs_school_js.svg" width="70" height="25" alt="">
         </img></a>
-      <span>2020 © English for Kids</span>
+      <span>2020 © Covid Dashboard</span>
       <a href="https://github.com/mary-jane77" className="stats">mary-jane77</a>
       <a href="https://github.com/Luck8989" className="stats">Luck8989</a>
     </p>
