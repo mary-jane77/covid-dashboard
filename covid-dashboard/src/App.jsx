9bbc4aea84dab7aa1d74ca5c367d159ebe01a527
Tatsiana <t-tanja@bk.ru>
2020-12-23 20:05:18 +0300

    conflict

--- covid-dashboard/src/App.jsx
+++ covid-dashboard/src/App.jsx
@@ -1,38 +1,90 @@
-import './App.css';
-import React, { useState } from 'react';
-import Table1 from './table1.jsx';
-import Table1p2 from './table1p2.jsx';
-import useFetch from './getInfo.jsx';
-import {
-  filterInfo,
-  periodInfo,
-  statusInfo,
-} from './table1Info';
-import findCountryInfo from './findCountryInfo';
+import React, { useState, useEffect, useCallback } from 'react';
+import MapView from './components/Map/MapView';
+import 'leaflet/dist/leaflet.css';
+import './CSS/App.scss';
+import axios from './axios';
+import Summary from './components/Summary.jsx';
+import DetailsView from './components/Map/DetailsView.jsx';
+import './components/Map.css';
 
 function App() {
-  const info = useFetch('https://api.covid19api.com/summary');
-  const [countryData, setCountry] = useState('Belarus');
+  const [locationArray, setLocationArray] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState(null);
+  const [mapCenter, setMapCenter] = useState([51.505, -0.09]);
+  const [loading, setLoading] = useState(false);
 
-  const filterData = (period, status) => filterInfo(info, period, status);
-  const findCountry = (country) => {
-    setCountry(country);
-  };
+  function sortedLocationArray(locations) {
+    // eslint-disable-next-line max-len
+    return [...locations].sort((location1, location2) => location2.latest.confirmed - location1.latest.confirmed);
+  }
+  const onSelectLocation = useCallback((id) => {
+    const location = locationArray.find((_location) => _location.id === id);
+    if (location === undefined) {
+      setSelectedLocation(null);
+      return;
+    }
+    setSelectedLocation(location);
+    const { coordinates: { latitude, longitude } } = location;
+    setMapCenter([latitude, longitude]);
+  }, [locationArray]);
 
+  const onDeselectLocation = useCallback(() => {
+    setSelectedLocation(null);
+  }, []);
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get('/v2/locations').then((res) => {
+      const sortedLocations = sortedLocationArray(res.data.locations);
+      setLoading(false);
+
+      if (res.status === 200) {
+        setLocationArray(sortedLocations);
+      }
+    })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  // console.log(locationArray);
+
+  if (loading) {
+    return <p> Fetching!!! </p>;
+  }
+
+  let detailsView = null;
+  if (selectedLocation != null) {
+    detailsView = <DetailsView location={selectedLocation} onClickClose={onDeselectLocation} />;
+  }
   return (
+
     <div className="App">
-      <header className="App-header">
-        COVID-19 Dashboard
-      </header>
-      {info && <Table1
-        filterData={filterData}
-        info={filterInfo(info, 0, 0)}
-        periodInfo={periodInfo}
-        findCountry={findCountry}
-        statusInfo={statusInfo}
-      />}
-      {info && <Table1p2 info={findCountryInfo(info, countryData)} />}
+
+    <div className="app__left">
+
+    <div className="app__header">
+    <h1>COVID-19 Dashboard by the Center for Systems Science and Engineering
+        (CSSE) at Johns Hopkins University (JHU)</h1>
+   </div>
+
+          <Summary
+        locationArray={locationArray}
+        selectedLocation={selectedLocation}
+        onSelectItem={onSelectLocation}
+        onDeselectItem={onDeselectLocation}
+          />
+
+    <div className='cards_map'>
+       <MapView
+      locationArray={locationArray}
+      mapCenter={mapCenter}
+      onSelectMarker={onSelectLocation} />
+      {detailsView}
     </div>
+
+       </div>
+ </div>
+
   );
 }
 
